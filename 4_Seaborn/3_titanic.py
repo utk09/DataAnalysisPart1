1e3f7f8ff8995df511168b20414404942b73bf50
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2019-01-04 00:54:40 +0530

    Update 3_titanic.py

--- 4_Seaborn/3_titanic.py
+++ 4_Seaborn/3_titanic.py
@@ -5,7 +5,7 @@ import seaborn as sns
 # Load data
 titanic = sns.load_dataset("titanic")
 
-# Set up a factorplot, now known as catplot
+# Set up a factorplot, now known as catplot()
 g = sns.catplot("class", "survived", "sex", data=titanic, kind="bar", palette="muted", legend=False)
 
 # colors should be displayed with the "palette" argument
